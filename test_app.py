f4f68aaeedacb1ad8c686b6cf5c154cdb1621b87
Kate <klhutch@ccs.neu.edu>
2015-02-08 00:14:51 -0500

    It sort of works-ish

--- test_app.py
+++ test_app.py
@@ -2,24 +2,45 @@
 from flask import Flask, jsonify, json, request, render_template
 import os
 import jinja2
-SITE_ROOT = os.path.realpath(os.path.dirname('app'))
-app = Flask(__name__)
+from flask.ext.assets import Environment, Bundle
+
+app = Flask(__name__, static_url_path='')
+
+assets = Environment(app)
+js = Bundle('jquery.js', 'base.js', 'widgets.js',
+            filters='jsmin', output='gen/packed.js')
+assets.register('js_all', js)
 # app.jinja_loader = jinja2.FileSystemLoader('app/templates') 
 
 @app.route("/dolphins/questions")
 def questions():
-    json_url = os.path.join(SITE_ROOT, "templates/Dolfin/QuestionSets", "Dolphin.json")
+    SITE_ROOT = os.path.realpath(os.path.dirname('Dolfin'))
+    json_url = os.path.join(SITE_ROOT, "QuestionSets", "Dolphin.json")
     data = json.load(open(json_url))
     return jsonify(data)
-
-
 
 @app.route("/")
 def home():
    return render_template("index.html")
 
+@app.route("/signup.html")
+def signup():
+	return render_template("signup.html")
+
+@app.route("/login.html")
+def login():
+	return render_template("login.html")
+
+@app.route("/topics.html")
+def topics():
+	return render_template("topics.html")
+
+@app.route("/about.html")
+def about():
+	return render_template("about.html")
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
 
 
